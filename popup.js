8dc943cf791727464add67171380e8eb18d2bef6
Akash Patel <Akash@v1022-rn-203-48.campus-dynamic.uwaterloo.ca>
2016-03-04 18:41:11 -0500

    progress
    i think..

--- popup.js
+++ popup.js
@@ -1,19 +1,26 @@
 var key = "AIzaSyB4DkQLaq2ZOXHnpuAsuUMJbh33WbZbA34";
 var serviceUrl="https://www.googleapis.com/urlshortener/v1/url?key="+key;
 
+//Call getUrl function once extension icon is clicked
+chrome.browserAction.onClicked.addListener(getUrl);
 
-// Get url of current tab when icon is clicked
+
+// Get url of current tab once extension icon is clicked and shorten it using shortenUrl function
+function getUrl(){
+  
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     shortenUrl(tabs[0].url);
-});
+});  
+    
+}
 
 
 //Call Google URL Shortener API to get shortened version of URL
-var shortenUrl = function(url) {
+function shortenUrl(url) {
  $.post(serviceUrl, {longUrl: url},
   function(data){
     console.log("data" + data.id);
   }, "json");
-  });
+}
 
     
