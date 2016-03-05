d8c101c44a5bed8aa605cdc01a814c04cc87916a
Akash Patel <Akash@v1022-rn-203-48.campus-dynamic.uwaterloo.ca>
2016-03-04 23:05:15 -0500

    this'll do

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
  var APIkey = "AIzaSyB4DkQLaq2ZOXHnpuAsuUMJbh33WbZbA34";
  var APIUrl = "https://www.googleapis.com/urlshortener/v1/url?key=" + APIkey;
 
+
  // Get url of current tab once extension icon is clicked and shorten it using shortenUrl function
  function getUrl() {
-
      chrome.tabs.query({
          'active': true,
          'lastFocusedWindow': true
@@ -16,33 +16,17 @@
 
  //Call Google URL Shortener API to get shortened version of URL
  function shortenUrl(url) {
-     $.post(APIUrl, {
-             longUrl: url
-         },
-         function (data) {
-            console.log("data: " + data.id);
-            document.getElementById("short-url").innerHTML = data.id;
+     $.post(APIUrl, {longUrl: url },function (data) {
+            console.log("data: " + data);
+            document.getElementById("short-url").innerHTML = data;
 
          }, "json");
  }
 
-getUrl();
-alert("Success!");
-//document.getElementById("copy-link").addEventListener("click", copyfunction);
 
 var btn = document.getElementById('copy-link');
 var clipboard = new Clipboard(btn);
 
-
-clipboard.on('success', function(e) {
-    console.info('Action:', e.action);
-    console.info('Text:', e.text);
-    console.info('Trigger:', e.trigger);
-    alert("Success!");
-    e.clearSelection();
-});
-
-clipboard.on('error', function(e) {
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
+$(document).ready(function() {
+    console.log("SWAAAG");
 });
