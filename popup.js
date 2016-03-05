d6d22f7fb0de9a23d6953d6fbc808b625d6d4521
Akash Patel <Akash@v1022-rn-203-48.campus-dynamic.uwaterloo.ca>
2016-03-04 19:27:26 -0500

    copying to clipboard

--- popup.js
+++ popup.js
@@ -1,26 +1,34 @@
-var key = "AIzaSyB4DkQLaq2ZOXHnpuAsuUMJbh33WbZbA34";
-var serviceUrl="https://www.googleapis.com/urlshortener/v1/url?key="+key;
+ var APIkey = "AIzaSyB4DkQLaq2ZOXHnpuAsuUMJbh33WbZbA34";
+ var APIUrl = "https://www.googleapis.com/urlshortener/v1/url?key=" + APIkey;
 
-//Call getUrl function once extension icon is clicked
-chrome.browserAction.onClicked.addListener(getUrl);
+ // Get url of current tab once extension icon is clicked and shorten it using shortenUrl function
+ function getUrl() {
+
+     chrome.tabs.query({
+         'active': true,
+         'lastFocusedWindow': true
+     }, function (tabs) {
+         shortenUrl(tabs[0].url);
+     });
+
+ }
 
 
-// Get url of current tab once extension icon is clicked and shorten it using shortenUrl function
-function getUrl(){
-  
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    shortenUrl(tabs[0].url);
-});  
-    
-}
+ //Call Google URL Shortener API to get shortened version of URL
+ function shortenUrl(url) {
+     $.post(APIUrl, {
+             longUrl: url
+         },
+         function (data) {
+            console.log("data: " + data.id);
+            document.getElementById("short-url").innerHTML = data.id;
 
+         }, "json");
+ }
 
-//Call Google URL Shortener API to get shortened version of URL
-function shortenUrl(url) {
- $.post(serviceUrl, {longUrl: url},
-  function(data){
-    console.log("data" + data.id);
-  }, "json");
-}
+getUrl();
 
-    
+//document.getElementById("copy-link").addEventListener("click", copyfunction);
+
+var btn = document.getElementById('copy-link');
+var clipboard = new Clipboard(btn);
