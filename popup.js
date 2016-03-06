6e25895667c940efd9ba30752d9c24164074bdea
Akash Patel <akash_patel97@hotmail.com>
2016-03-05 21:16:13 -0500

    moved clipboard stuff to html file

--- popup.js
+++ popup.js
@@ -1,63 +1,48 @@
  var APIkey = "AIzaSyB4DkQLaq2ZOXHnpuAsuUMJbh33WbZbA34";
  var APIUrl = "https://www.googleapis.com/urlshortener/v1/url?key=" + APIkey;
 
-var btn = document.getElementById("copy-link");
-var clipboard = new Clipboard(btn);
-
- clipboard.on('success', function(e) {
-        console.log(e);
-    });
-
-    clipboard.on('error', function(e) {
-        console.log(e);
-    });
-
  // Get url of current tab once extension icon is clicked and shorten it using shortenUrl function
 
-/* function getUrl() {
-     chrome.tabs.query(
-    {
-         'active': true,
-         'currentWindow': true
-     }, 
-    function (tabs) {
-        var currUrl = tabs[0].url; 
-         shortenUrl(currUrl);
-        console.log(currUrl);
-        alert(currUrl);
-     });
+ /* function getUrl() {
+      chrome.tabs.query(
+     {
+          'active': true,
+          'currentWindow': true
+      }, 
+     function (tabs) {
+         var currUrl = tabs[0].url; 
+          shortenUrl(currUrl);
+         console.log(currUrl);
+         alert(currUrl);
+      });
 
- }*/
+  }*/
 
 
-function getUrl(){
-    var currUrl = window.location.href;
-    shortenUrl(currUrl);
-    console.log(currUrl);
-}
+ function getUrl() {
+     var currUrl = window.location.href;
+     shortenUrl(currUrl);
+     console.log(currUrl);
+ }
 
-//Call Google URL Shortener API to get shortened version of URL
+ //Call Google URL Shortener API to get shortened version of URL
  function shortenUrl(url) {
-     $.post(APIUrl, {longUrl: url },function (data) {
+     $.post(APIUrl, {
+         longUrl: url
+     }, function (data) {
          console.log(data);
          alert(data);
          alert(data.id);
-            console.log("data: " + data.id);
-            document.getElementById("short-url").innerHTML = data.id;
+         console.log("data: " + data.id);
+         document.getElementById("short-url").innerHTML = data.id;
 
-         }, "json");
+     }, "json");
  }
 
-document.addEventListener("DOMContentLoaded", getUrl);
+ document.addEventListener("DOMContentLoaded", getUrl);
 
-$document.ready(function(){
-    console.log("READY!!");
-    getUrl();
-    alert("IS THIS THING ON?");
-});
-
-
-
-
-
-
+ $document.ready(function () {
+     console.log("READY!!");
+     getUrl();
+     alert("IS THIS THING ON?");
+ });
