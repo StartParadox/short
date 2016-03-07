33f353f9d849c763c2379169a0531d8c6d5a613d
Akash Patel <akash_patel97@hotmail.com>
2016-03-06 20:02:07 -0500

    moved script to html

--- popup.js
+++ popup.js
@@ -1,5 +1,8 @@
+ /*
  var APIkey = "AIzaSyB4DkQLaq2ZOXHnpuAsuUMJbh33WbZbA34";
- var APIUrl = "https://www.googleapis.com/urlshortener/v1/url?key=" + APIkey;
+  var APIUrl = "https://www.googleapis.com/urlshortener/v1/url?key=" + APIkey;
+ */
+
 
  // Get url of current tab once extension icon is clicked and shorten it using shortenUrl function
 
@@ -18,25 +21,26 @@
 
   }*/
 
+ /*
+  function getUrl() {
+      var currUrl = window.location.href;
+      shortenUrl(currUrl);
+      console.log(currUrl);
+  }
 
- function getUrl() {
-     var currUrl = window.location.href;
-     shortenUrl(currUrl);
-     console.log(currUrl);
- }
+  //Call Google URL Shortener API to get shortened version of URL
+  function shortenUrl(url) {
+      $.post(APIUrl, {
+          longUrl: url
+      }, function (data) {
+          console.log(data);
+          alert(data);
+          alert(data.id);
+          console.log("data: " + data.id);
+          document.getElementById("short-url").innerHTML = data.id;
 
- //Call Google URL Shortener API to get shortened version of URL
- function shortenUrl(url) {
-     $.post(APIUrl, {
-         longUrl: url
-     }, function (data) {
-         console.log(data);
-         alert(data);
-         alert(data.id);
-         console.log("data: " + data.id);
-         document.getElementById("short-url").innerHTML = data.id;
+      }, "json");
+  }
 
-     }, "json");
- }
-
- document.addEventListener("DOMContentLoaded", getUrl);
+  document.addEventListener("DOMContentLoaded", getUrl);
+ */
