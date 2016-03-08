6caf059495eb3b230628cf8cf3cd74acff4a63f7
Akash Patel <akash_patel97@hotmail.com>
2016-03-07 21:07:20 -0500

    backup
    deleting js file again..

--- popup.js
+++
@@ -1,5 +0,0 @@
- var butt = document.getElementById("copy-url");
-
-        butt.addEventListener("click", function () {
-            alert("THIS DOESN'T WORK YET, PLEASE HIGHLIGHT THE URL AND USE CTRL+C TO COPY.");
-        });
