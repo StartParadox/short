a34c562550a0dc4961b9c9f584bd5e4d1502549a
Akash Patel <akash_patel97@hotmail.com>
2016-03-07 21:02:25 -0500

    stuff

---
+++ popup.js
@@ -0,0 +1,5 @@
+ var butt = document.getElementById("copy-url");
+
+        butt.addEventListener("click", function () {
+            alert("THIS DOESN'T WORK YET, PLEASE HIGHLIGHT THE URL AND USE CTRL+C TO COPY.");
+        });
